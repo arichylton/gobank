0baac2923c0e2b6de9f0e9e2c5b5baf07014d862
Aric Hylton <Arich212@gmail.com>
2025-04-07 22:10:40 -0500

    added form component and restructured create user function

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,22 +1,15 @@
 // import { Routes, Route } from "react-router-dom";
-import { useEffect } from "react";
 import { Navbar } from "../components/navbar";
-import { getUsers } from "../api";
-import { UserInput } from "../types";
+import { InputForm } from "../components/form";
 function App() {
-    useEffect(() => {
-        const newUser: UserInput = {
-            name: "Aric",
-            email: "Arich212@gmail.com",
-            password: "pa55word",
-        };
-        getUsers(newUser);
-    }, []);
     return (
         <div className="max-w-4xl mx-auto px-4">
             <Navbar />
             <main className="mt-6">
                 <h1 className="text-2xl font-bold">Welcome to My App</h1>
+                <div className="mt-4 w-1/3">
+                    <InputForm />
+                </div>
             </main>
         </div>
     );
---
+++ src/components/form/inputForm.tsx
@@ -0,0 +1,63 @@
+import { useState } from "react";
+import { getUsers } from "../../api";
+import { UserInput } from "../../types";
+
+export const InputForm = () => {
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSubmit = (e: React.FormEvent) => {
+        e.preventDefault(); // Prevent page reload
+        const newUser: UserInput = {
+            name: "Aric",
+            email: "Arich212@gmail.com",
+            password: "pa55word",
+        };
+        getUsers(newUser);
+    };
+
+    return (
+        <form onSubmit={handleSubmit} className="flex flex-col justify-center">
+            <div>
+                <label htmlFor="name">Name:</label>
+                <input
+                    id="name"
+                    type="text"
+                    name="name"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    className="bg-white ms-2 mb-2 text-black"
+                />
+            </div>
+            <div>
+                <label htmlFor="email">Email:</label>
+                <input
+                    id="email"
+                    type="email"
+                    name="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    className="bg-white ms-2 mb-2 text-black"
+                />
+            </div>
+            <div>
+                <label htmlFor="password">Password:</label>
+                <input
+                    id="password"
+                    type="password"
+                    name="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    className="bg-white ms-2 mb-2 text-black"
+                />
+            </div>
+            <button
+                className="bg-black w-1/3 text-white p-2 rounded"
+                type="submit"
+            >
+                Submit
+            </button>
+        </form>
+    );
+};
