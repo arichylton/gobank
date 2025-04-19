cf37c0edab69ed3da996d856e22e9cd20546c695
Aric Hylton <Arich212@gmail.com>
2025-04-18 20:35:56 -0500

    updated ui in main

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -6,7 +6,7 @@ function App() {
     return (
         <main className="max-w-4xl mx-auto px-4">
             <Navbar />
-            <div className="mt-3">
+            <div className="mt-2">
                 <Routes>
                     <Route path="/" element={<HomePage />} />
                     <Route path="/login" element={<LoginPage />} />
